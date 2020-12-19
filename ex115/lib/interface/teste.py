4161801341f3c9af160289ec5b54b3c46325c392
Rivan-Roque <62901347+Rivan-Roque@users.noreply.github.com>
2020-12-19 12:49:49 -0100

    Quarto comite
    Soma das variaveis

--- ex115/lib/interface/teste.py
+++ ex115/lib/interface/teste.py
@@ -1,3 +1,5 @@
 print('Olá Mundo!')
 numero1 = int(input('Primeiro Numero: '))
 numero2 = int(input('Segundo Numero: '))
+total = numero1 + numero2
+print(f'A total entre {numero1} e {numero2} equivale a {total}')
